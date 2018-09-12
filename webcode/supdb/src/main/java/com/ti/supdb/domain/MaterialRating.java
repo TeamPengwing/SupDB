a778e23e6c42a1fd4246cff652b913938de4e409
Maureen Yogyog <m-yogyog@ti.com>
2018-09-12 11:33:16 +0800

    material rating object

---
+++ webcode/supdb/src/main/java/com/ti/supdb/domain/MaterialRating.java
@@ -0,0 +1,41 @@
+package com.ti.supdb.domain;
+
+public class MaterialRating {
+
+    private int materialId;
+    private String materialName;
+    private String ratingValue;
+    private String ratingType;
+
+    public int getMaterialId() {
+        return materialId;
+    }
+
+    public void setMaterialId(int materialId) {
+        this.materialId = materialId;
+    }
+
+    public String getMaterialName() {
+        return materialName;
+    }
+
+    public void setMaterialName(String materialName) {
+        this.materialName = materialName;
+    }
+
+    public String getRatingValue() {
+        return ratingValue;
+    }
+
+    public void setRatingValue(String ratingValue) {
+        this.ratingValue = ratingValue;
+    }
+
+    public String getRatingType() {
+        return ratingType;
+    }
+
+    public void setRatingType(String ratingType) {
+        this.ratingType = ratingType;
+    }
+}
