2a538bd81268dbfe0177b465603d3c4f63b22fb1
Maureen Yogyog <m-yogyog@ti.com>
2018-09-13 13:12:15 +0800

    comparator changes

---
+++ webcode/supdb/src/main/java/com/ti/supdb/domain/CompDataFieldValue.java
@@ -0,0 +1,113 @@
+package com.ti.supdb.domain;
+
+public class CompDataFieldValue {
+
+    private int dataGroupId;
+    private String dataGroup;
+    private int dataFieldId;
+    private String dataField;
+    private int matId1;
+    private int matId2;
+    private int matId3;
+    private int matId4;
+    private String dataFieldValueMat1;
+    private String dataFieldValueMat2;
+    private String dataFieldValueMat3;
+    private String dataFieldValueMat4;
+
+    public int getDataGroupId() {
+        return dataGroupId;
+    }
+
+    public void setDataGroupId(int dataGroupId) {
+        this.dataGroupId = dataGroupId;
+    }
+
+    public String getDataGroup() {
+        return dataGroup;
+    }
+
+    public void setDataGroup(String dataGroup) {
+        this.dataGroup = dataGroup;
+    }
+
+    public int getDataFieldId() {
+        return dataFieldId;
+    }
+
+    public void setDataFieldId(int dataFieldId) {
+        this.dataFieldId = dataFieldId;
+    }
+
+    public String getDataField() {
+        return dataField;
+    }
+
+    public void setDataField(String dataField) {
+        this.dataField = dataField;
+    }
+
+    public int getMatId1() {
+        return matId1;
+    }
+
+    public void setMatId1(int matId1) {
+        this.matId1 = matId1;
+    }
+
+    public int getMatId2() {
+        return matId2;
+    }
+
+    public void setMatId2(int matId2) {
+        this.matId2 = matId2;
+    }
+
+    public int getMatId3() {
+        return matId3;
+    }
+
+    public void setMatId3(int matId3) {
+        this.matId3 = matId3;
+    }
+
+    public int getMatId4() {
+        return matId4;
+    }
+
+    public void setMatId4(int matId4) {
+        this.matId4 = matId4;
+    }
+
+    public String getDataFieldValueMat1() {
+        return dataFieldValueMat1;
+    }
+
+    public void setDataFieldValueMat1(String dataFieldValueMat1) {
+        this.dataFieldValueMat1 = dataFieldValueMat1;
+    }
+
+    public String getDataFieldValueMat2() {
+        return dataFieldValueMat2;
+    }
+
+    public void setDataFieldValueMat2(String dataFieldValueMat2) {
+        this.dataFieldValueMat2 = dataFieldValueMat2;
+    }
+
+    public String getDataFieldValueMat3() {
+        return dataFieldValueMat3;
+    }
+
+    public void setDataFieldValueMat3(String dataFieldValueMat3) {
+        this.dataFieldValueMat3 = dataFieldValueMat3;
+    }
+
+    public String getDataFieldValueMat4() {
+        return dataFieldValueMat4;
+    }
+
+    public void setDataFieldValueMat4(String dataFieldValueMat4) {
+        this.dataFieldValueMat4 = dataFieldValueMat4;
+    }
+}
